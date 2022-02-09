01f825c2f063f291ac259aaf6a0dd182bf7576a6
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-09 00:38:14 -0800

    add Blog.js and Comment.js and add more relationships in Models's index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,33 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
+
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Blog.belongsTo(User, {
-    foreignKey: 'user_id'
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
-module.exports = { User, Blog };
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+
+
+module.exports = { User, Blog, Comment };
