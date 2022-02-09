5f61a7284ca8a1b37aa45f243ab78da472e4433e
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-08 22:14:13 -0800

    add beginning of handlebars main page with header and title

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//Set up Handlevars with helpers
+const hbs = exphbs.create({ helpers });
+
+//Set up express port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars for express
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
